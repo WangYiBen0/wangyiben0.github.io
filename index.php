c0cc5b7bfe25a7ce28baf4ff3b71503219c86706
WangYiben0 <wangyiben1@outlook.com>
2024-09-19 23:10:49 +0800

    update index.php

--- index.php
+++ index.php
@@ -9,12 +9,7 @@
 <>
 	<h1>欢迎来到我的个人主页</h1>
 	<p>只是一个初三牲的Github Page罢了</p>
-	<?php
-		// 获取当前时间
-		$now = time();
-		echo "Now is ";
-		echo $now;
-	?>
+	<p>为什么Github Page不支持PHP啊！</p>
 </body>
 
 </html>
